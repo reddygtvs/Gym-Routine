ece82355829108ac919bbce45a4ff33933830029
Anannya Mahapatra <anannya1947@gmail.com>
2021-08-06 17:10:39 +0530

    added basic CSS elements

--- script.js
+++ script.js
@@ -1,36 +1,57 @@
 var weight;
 var document;
-var reps;
-var list;
-var element = 0;
+var element = 0, c=0;
 
 
 function returnWeight() {
     weight = document.getElementById("weight").value;
-    document.getElementById("message").innerHTML = "Hi there, your 1RM weight is: " + weight;
+    document.getElementById("message").innerHTML = "Hi there, your 1RM weight is: " + weight + "Kg";
     
-    reps = ['5 reps (65%): ', '5 reps (75%): ', '5+ reps (85%): ', '3 reps (70%): ', '3 reps (80%): ', '3+ reps (90%): ', '5 reps (75%): ', '3 reps (85%): ', '1+ reps (95%): '];
-    percentages = [65, 75, 85, 70, 80, 90, 75, 85, 95];
-    editArray(reps, percentages);
-    document.getElementById("output").innerHTML = "";
-    list = document.getElementById("output");
-    
-    reps.forEach((item) => {
+    reps1 = ['5 reps (65%):  ', '5 reps (75%): ', '5+ reps (85%): '];
+    reps2 = ['3 reps (70%): ', '3 reps (80%): ', '3+ reps (90%): '];
+    reps3 = ['5 reps (75%): ', '3 reps (85%): ', '1+ reps (95%): '];
+    percentages1 = [.65, .75, .85];
+    percentages2 = [.70, .80, .90]; 
+    percentages3 = [.75, .85, .95];
+
+    editArray(reps1, percentages1);
+    document.getElementById("week-heading1").innerHTML="Week 1";
+    document.getElementById("output1").innerHTML = "";
+    list1 = document.getElementById("output1");
+
+    reps1.forEach((item) => {
         let li = document.createElement("li");
         li.innerHTML = item;
-        list.appendChild(li);
+        list1.appendChild(li);
     })
     
-    return false;
+    editArray(reps2, percentages2);
+    document.getElementById("week-heading2").innerHTML="Week 2";
+    document.getElementById("output2").innerHTML = "";
+    list2 = document.getElementById("output2");
 
+    reps2.forEach((item) => {
+        let li = document.createElement("li");
+        li.innerHTML = item;
+        list2.appendChild(li);
+    })
+    
+    editArray(reps3, percentages3);
+    document.getElementById("week-heading3").innerHTML="Week 3";
+    document.getElementById("output3").innerHTML = "";
+    list3 = document.getElementById("output3");
+
+    reps3.forEach((item) => {
+        let li = document.createElement("li");
+        li.innerHTML = item;
+        list3.appendChild(li);
+    })
+     return false;
 }
-function calcPercentage(percent, weight) {
-    let w = percent * weight;
-    return w / 100.0;
-}
+
 function editArray(reps, percentages) {
     let n = reps.length;
     for (var i = 0; i < n; i++) {
-        reps[i] = reps[i] + calcPercentage(percentages[i], weight) + " Kg";
+        reps[i] = reps[i] + Math.round(percentages[i]*weight) + " Kg";
     }
 }
